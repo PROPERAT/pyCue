42f3637e2cbb352bd0f5f7580dc50661890a784a
PROPERAT <PROPERAT@properat-81cc9d.(none)>
2012-06-10 03:19:34 +0300

    Fixing searching yet again.

--- SceneManager.py
+++ SceneManager.py
@@ -18,11 +18,13 @@ class SceneManager:
     def add_scene_node(self, node, parent=0):
         if parent == 0:
             self.root_node.add_child(node)
+            node.set_parent(self.root_node)
         else:
             if self.get_node_from_ID(parent.get_ID()) is None:
                 raise SceneManagerError("Could not add node with parent who is not in the scene !")
             else:
                 parent.add_child(node)
+                node.set_parent(self.root_node)
     
     def get_root_node(self):
         return self.root_node
@@ -32,24 +34,24 @@ class SceneManager:
             start = self.root_node
         
         if start.get_ID() == id:
-            return self.root_node
+            return start
         
         else:
             children_list = start.get_children()
             for child in children_list:
-                get_node_fromID(self, id, child)
+                return self.get_node_from_ID(id, child)
     
     def get_node_from_name(self, name, start=0):
         if start == 0:
             start = self.root_node
         
         if start.get_name() == name:
-            return self.root_node
+            return start
         
         else:
             children_list = start.get_children()
             for child in children_list:
-                get_node_fromID(self, name, child)
+                return self.get_node_from_name(name, child)
     
     def remove_all(self):
         self.root_node.remove_all()
