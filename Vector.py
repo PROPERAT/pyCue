d34acf1dfdb4b387bce3bc37f7aa7ea59b86f6bb
PROPERAT <PROPERAT@properat-81cc9d.(none)>
2012-06-10 17:40:20 +0300

    Syntax shugar for vertex, cross product and misc. fixes.

--- Vector.py
+++ Vector.py
@@ -12,6 +12,17 @@ class vector(ndarray):
         for pos,val in enumerate(args[0]):
             ndarray.__getitem__(self,pos).__setitem__(0,val)
     
+    def __getattr__(self, attr):
+        if attr == 'x':
+            return self[0]        
+        elif attr == 'y':
+            return self[1]
+        elif attr == 'z':
+            return self[2]
+        elif attr == 'w':
+            return self[3]
+        else:
+            raise AttributeError("No such attribute",attr)
     
     def __array_finalize__(self, obj):
         pass
@@ -30,3 +41,7 @@ class vector(ndarray):
     
     def dot(self, other):
         return self[0]*other[0] + self[1]*other[1] + self[2]*other[2]
+        
+    def cross(self, other):
+        return vector([self.y*other.z - self.z*other.y, self.z*other.x - self.x*other.z, self.x*other.y - self.y*other.x])
+        
